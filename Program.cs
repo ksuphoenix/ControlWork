5bf273dea40d438cbd212fcfa54bccfa715ba4f5
ksuphoenix <ksuphoenix@gmail.com>
2022-12-28 13:39:09 +0400

    readme

--- Program.cs
+++ Program.cs
@@ -1,1 +1,38 @@
-﻿
+﻿using System;
+using static System.Console;
+
+Clear();
+string [] array = new string[] {"All", "you", "need", "is", "love", "<3"}; 
+//string [] NewArray = new string[array.Length];
+
+PrintArray(array);
+WriteLine();
+string [] NewArray = ChangeStringArray(array);
+PrintArray(NewArray);
+
+
+
+string [] ChangeStringArray(string[] array)
+{
+    string [] NewArray = new string[array.Length];
+    int j = 0; 
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            NewArray[j] = array[i];
+            j++;
+        }
+    }
+    return NewArray;
+}
+
+
+void PrintArray(string[] inArray)
+{
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        Write($"{inArray[i]} ");
+    }
+    WriteLine();
+}
